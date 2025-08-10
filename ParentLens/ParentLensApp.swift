e7e9542088a70c964e82d3ed4dfbe2931129e131
insitektalay <tm30junglehouse@gmail.com>
2025-08-10 12:41:05 +0700

    Initial Commit

---
+++ ParentLens/ParentLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentLensApp.swift
+//  ParentLens
+//
+//  Created by alex kerss on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParentLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParentLensTests/ParentLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParentLensTests.swift
+//  ParentLensTests
+//
+//  Created by alex kerss on 10/08/2025.
+//
+
+import Testing
+@testable import ParentLens
+
+struct ParentLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
